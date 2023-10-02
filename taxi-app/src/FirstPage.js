8d91181d2f47a342ab874e6c78b53d5213b3efb6
sdh77 <141728845+sdh77@users.noreply.github.com>
2023-10-02 16:07:27 +0900

    .

---
+++ taxi-app/src/FirstPage.js
@@ -0,0 +1,18 @@
+function FirstPage() {
+  return (
+    <div className="firstPage">
+      <div className="firstPage_img">
+        <img
+          className="logo"
+          src="https://pimg.designhouse.co.kr/cms/contents/direct/info_id/45267/1220841607201.jpg"
+        ></img>
+      </div>
+      <div className="firstPage_btn">
+        <button className="firstPage_btn__logIn">LogIn</button>
+        <button className="firstPage_btn__singUp">SingUp</button>
+      </div>
+    </div>
+  );
+}
+
+export default FirstPage;
