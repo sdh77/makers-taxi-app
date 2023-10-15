5a905ae9bae799f53f30970a87c5878a726f5ef2
sdh77 <141728845+sdh77@users.noreply.github.com>
2023-10-15 21:51:53 +0900

    .

---
+++ PHP/singUp.php
@@ -0,0 +1,6 @@
+<?php
+$conn = pg_connect('host=localhost port=5432 dbname=ilprimo user=hanium_kioski password=aaa') or die('Could not connect: ' . pg_last_error());
+pg_query($conn, $sql);
+pg_close($conn);
+
+?>
