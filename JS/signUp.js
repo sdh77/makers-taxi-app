72f38bafe296cddf6045b69b8433dd48cd3c333b
sdh77 <141728845+sdh77@users.noreply.github.com>
2023-10-16 18:45:49 +0900

    로그인 페이지 form방식

--- JS/signUp.js
+++
@@ -1,7 +0,0 @@
-const signUp = document.querySelector(".signUpbtn");
-
-function handleSignUpSubmit(event) {
-  event.preventDefault();
-}
-
-signUp.addEventListener("submit", handleSignUpSubmit);
--- PHP/signUp.php
+++ PHP/signUp.php
@@ -11,9 +11,20 @@ $loginpwd = $_POST['loginpwd'];
 $sql = "INSERT INTO taxi_userinfo(id, name, pwd, student_ID, account, nickname, phonenumber)
 VALUES ('" . $loginid . "', '" . $name . "','" . $loginpwd . "','" . $id . "','" . $account . "','" . $nickname . "'," . $phone . ")";
 // echo $sql;
-pg_query($conn, $sql);
+$result = pg_query($conn, $sql);
+if ($result) {
+  echo '<script type="text/javascript">
+  alert("회원가입 성공");
+  window.location.href ="../index.html";
+  </script>';
+} else {
+  echo '<script type="text/javascript">
+alert("회원가입 실패");
+window.location.href ="../signUp.html";
+</script>';
+}
 pg_close($conn);
 
-echo file_get_contents("../HTML/index.html");
+
+
 ?>
-<link rel="stylesheet" href="../CSS/styles.css" />
