6585ca3194d7431d5706eb815ef3eef8dc4f5523
sdh77 <141728845+sdh77@users.noreply.github.com>
2023-10-16 17:32:05 +0900

    post

--- PHP/signUp.php
+++ PHP/signUp.php
@@ -1,17 +1,19 @@
 <?php
 $conn = pg_connect('host=localhost port=5432 dbname=tanyang user=sanggeukz password=taxi') or die('Could not connect: ' . pg_last_error());
-$name = $_GET['name'];
-$id = $_GET['id'];
-$account = $_GET['account'];
-$nickname = $_GET['nickname'];
-$phone = $_GET['phone'];
-$loginid = $_GET['loginid'];
-$loginpwd = $_GET['loginpwd'];
-$sql = "INSERT INTO taxi_userinfo(id, name, pwd, pwd, student_ID, account, nickname, phonenumber)
-VALUES ('".$id."', '".$name."','".$pwd."','".$student_ID."','".$account."','".$nickname."',".$phonenumber.")"
-echo $sql; 
 
+$name = $_POST['name'];
+$id = $_POST['id'];
+$account = $_POST['account'];
+$nickname = $_POST['nickname'];
+$phone = $_POST['phone'];
+$loginid = $_POST['loginid'];
+$loginpwd = $_POST['loginpwd'];
+$sql = "INSERT INTO taxi_userinfo(id, name, pwd, student_ID, account, nickname, phonenumber)
+VALUES ('" . $loginid . "', '" . $name . "','" . $loginpwd . "','" . $id . "','" . $account . "','" . $nickname . "'," . $phone . ")";
+// echo $sql;
 pg_query($conn, $sql);
 pg_close($conn);
 
+echo file_get_contents("../HTML/index.html");
 ?>
+<link rel="stylesheet" href="../CSS/styles.css" />
