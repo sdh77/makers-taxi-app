43c18ee9d86877dead8fd50000d1453ced1076d6
sdh77 <141728845+sdh77@users.noreply.github.com>
2023-10-23 13:31:51 +0900

    folder

--- form-php/PHP/checklogin.php
+++ form-php/PHP/checklogin.php
@@ -1,5 +1,6 @@
 <?php
-$conn = pg_connect('host=localhost port=5432 dbname=tanyang user=sanggeukz password=taxi') or die('Could not connect: ' . pg_last_error());
+$conn = pg_connect('host=localhost port=5432 dbname=tanyang user=sanggeukz password=taxi')
+  or die('Could not connect: ' . pg_last_error());
 
 $id = $_POST['id'];
 $pwd = $_POST['pwd'];
@@ -10,27 +11,27 @@ if ($result) {
   if (pg_num_rows($result) > 0) {
     while ($row = pg_fetch_assoc($result)) {
       if ($row['pwd'] == $pwd) {
-        echo "로그인 완료";
+        // echo "로그인 완료";
         echo "
         <script type='text/javascript'>
-        window.location.href = '../#'
+        window.location.href = '../mainPage.html';
         </script>";
 
       } else {
-        echo "비밀번호를 확인하세요";
+        // echo "비밀번호를 확인하세요";
         echo "
         <script type='text/javascript'>
         alert('비밀번호를 확인하세요');
-        window.location.href = '../loginPage.html'
+        window.location.href = '../loginPage.html';
         </script>";
       }
     }
   } else {
-    echo "아이디 정보가 없습니다";
+    // echo "아이디 정보가 없습니다";
     echo "
         <script>
         alert('아이디 정보가 없습니다.');
-        window.location.href = '../loginPage.html'
+        window.location.href = '../loginPage.html';
         </script>";
   }
 }
