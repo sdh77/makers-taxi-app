f1bc71f19b67e79126457886a6a088e5ffeef76e
sdh77 <141728845+sdh77@users.noreply.github.com>
2023-11-26 20:23:42 +0900

    공지 업데이트!

--- src/PHP/showNotice.php
+++ src/PHP/showNotice.php
@@ -1,20 +1,30 @@
 <?php
 $conn = pg_connect('host=localhost port=5432 dbname=tanyang user=sanggeukz password=taxi')
   or die('Could not connect: ' . pg_last_error());
-
+$noticeSql = "select * from notice";
+$noticeDatas = pg_query($conn, $noticeSql);
+// echo $noticeSql;
 echo '<div class="main-top">
         <p>Notice</p>
         <button class="notice-closeBtn"><i class="fa-solid fa-x"></i></button>
       </div>';
 echo '<div class="main-middle">';
 echo '  <div class="notice-area">';
-echo '    <div class="notice-row">
-            <div class="notice-txt">
-              <i class="fa-solid fa-bullhorn"></i>
-              <div> 공지 내용</div>
-            </div>
-            <div class="notice-date"> 0000.00.00</div>
-          </div>';
+if ($noticeDatas) {
+  if (pg_num_rows($noticeDatas) > 0) {
+    while ($noticeData = pg_fetch_assoc($noticeDatas)) {
+      echo '<div class="notice-row">
+              <div class="notice-txt">
+                <i class="fa-solid fa-bullhorn"></i>
+                <div class="notice-title">' . $noticeData['noticetitle'] . '</div>
+                <div class="notice-title__txt">' . $noticeData['noticetxt'] . '</div>
+              </div>
+              <div class="notice-date">' . $noticeData['noticedate'] . '</div>
+            </div>';
+    }
+  }
+}
+
 echo '  </div>';
 echo '</div>';
 pg_close($conn);
