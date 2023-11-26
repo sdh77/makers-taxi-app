50b70ad87503867be7e78d4adf8501418900bde6
yangseee <138835385+yangseee@users.noreply.github.com>
2023-11-26 20:42:56 +0900

    되는 코드

--- src/JS/EntranceRoom.js
+++ src/JS/EntranceRoom.js
@@ -36,6 +36,7 @@ function enterRoom() {
   }).done(function (data) {
     main_topMiddle.innerHTML = data;
 
+    /* chat.js */
     ("use strict");
 
     class LiModel {
@@ -49,11 +50,11 @@ function enterRoom() {
         const li = document.createElement("li");
         li.classList.add(nickname.value === this.name ? "sent" : "received");
         const dom = `<span class="profile">
-          <span class="user">${this.name}</span>
-          <img class="image" src="https://placeimg.com/50/50/any" alt="any" />
-        </span>
-        <span class="message">${this.msg}</span>
-        <span class="time">${this.time}</span>`;
+      <span class="user">${this.name}</span>
+      <img class="image" src="https://placeimg.com/50/50/any" alt="any" />
+    </span>
+    <span class="message">${this.msg}</span>
+    <span class="time">${this.time}</span>`;
         li.innerHTML = dom;
         chatList.appendChild(li);
       }
@@ -65,8 +66,9 @@ function enterRoom() {
     });
 
     let liModelInstance;
-    const displayContainer = document.querySelector(".display-container");
+    const displayContainer = document.querySelector(".display-container"); // displayContainer 추가
 
+    // 서버로부터 LiModel을 받아오기
     socket.on("getLiModel", (receivedLiModel) => {
       liModelInstance = new LiModel(
         receivedLiModel.name,
@@ -74,6 +76,7 @@ function enterRoom() {
         receivedLiModel.time
       );
 
+      // LiModel을 받아왔으면 이제 채팅 메시지를 받을 수 있도록 리스너 등록
       socket.on("chatting", (data) => {
         appendChatMessage(data);
       });
@@ -89,7 +92,6 @@ function enterRoom() {
         name: nickname.value,
         msg: chatInput.value,
         time: new Date().toLocaleTimeString(),
-        chatId: chatId,
       };
       socket.emit("chatting", param);
     });
@@ -100,6 +102,7 @@ function enterRoom() {
         return;
       }
 
+      // LiModel 클래스를 사용하여 객체 생성
       const liModel = new LiModel(data.name, data.msg, data.time);
       liModel.makeLi();
       scrollToBottom();
